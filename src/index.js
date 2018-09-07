d6bd0f34d530ce2147e1f3fb7412aa1260b7aae9
Eva Royo <e.r.educacion@gmail.com>
2018-09-07 13:22:18 +0200

    cambios para la version 1.1.0, ordenar por nombre

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const cervezas = require('./cervezas.json')
 const uniqueRandomArray = require('unique-random-array')
-
+const _ = require('lodash')
 module.exports = {
-  todas: cervezas,
+  todas: _.sortBy(cervezas, ['nombre']),
   alazar: uniqueRandomArray(cervezas)
 }
